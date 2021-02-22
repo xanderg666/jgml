08be72681e9a8da7cbfadcdc948bea7f0fcfd2fe
Jorge Galan <jorge.a.galan@gmail.com>
2021-02-22 16:30:37 -0500

    add for y if

--- flask/templates.py
+++ flask/templates.py
@@ -10,17 +10,17 @@ def hello_world(): # requiere una funcion
     return render_template('index.html')
 
 
-@app.route('/user/<nn>')
+@app.route('/user1/<nn>')
 def usernn(nn='Invitado'):
     return render_template('user.html',nombre = nn)
 
 
-# @app.route('/user/<name>') #la varable name usa para poder capturar desde la barra
-# def user(name='Jorge'): # Por defecto el nombre
-#     age = 19
-#     my_list = [1,2,3,4]
-#     return render_template('user.html',name = name, age = age,list = my_list)#en el reder se pone la var a enviar al html
-#
+@app.route('/user/<name>') #la varable name usa para poder capturar desde la barra
+def user(name='Jorge'): # Por defecto el nombre
+    age = 19
+    my_list = [1,2,3,4]
+    return render_template('user.html',name = name, age = age,list = my_list)#en el reder se pone la var a enviar al html
+
 
 if __name__ == '__main__':
     app.run(debug=True,port=8000) # debug y puerto
