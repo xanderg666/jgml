99b5d8e080f39b0e37915f7244f1b75b0e93e3fa
Jorge Galan <jorge.a.galan@gmail.com>
2021-02-22 21:08:02 -0500

    se agrega las herencias

---
+++ flask/Herencias/extends.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)#nuevo objeto con name
+
+
+@app.route('/') #recibe un decorador con un string, la ruta es base
+def index():
+    name = 'Invitado'
+    return render_template('index.html',name=name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=8000) # debug y puerto
