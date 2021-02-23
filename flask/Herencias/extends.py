d93951353381a4b13499d8d5f5bad38048c4f633
Jorge Galan <jorge.a.galan@gmail.com>
2021-02-22 21:29:11 -0500

    se agrega las herencias clientes

--- flask/Herencias/extends.py
+++ flask/Herencias/extends.py
@@ -9,6 +9,11 @@ def index():
     name = 'Invitado'
     return render_template('index.html',name=name)
 
+@app.route('/client') #recibe un decorador con un string, la ruta es base
+def client():
+    list_name = ['Test1', 'Test2', 'Test3']
+    return render_template('client.html',list + list_name)
+
 
 if __name__ == '__main__':
     app.run(debug=True,port=8000) # debug y puerto
