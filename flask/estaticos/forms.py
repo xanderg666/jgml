702558c72e417b371a463a23864fc6ce96401731
Jorge Galan <jorge.a.galan@gmail.com>
2021-03-02 17:53:42 -0500

    csfr, se anade formulario de login protegido

--- flask/estaticos/forms.py
+++ flask/estaticos/forms.py
@@ -1,28 +1,38 @@
-from wtforms import Form, StringField, TextField
+from wtforms import Form, StringField, TextField, PasswordField
 from wtforms.fields.html5 import EmailField
 # import email_validator
 
 
-#validaciones de formulario
+# validaciones de formulario
 from wtforms import validators
 
-def length_honeypot(form,field):
+
+def length_honeypot(form, field):
     if len(field.data) > 0:
         raise validators.ValidationError('El campo debe estar vacio')
 
+
 class CommentForm(Form):
-    #validaciones , usa un segundo parametro de string []
+    # validaciones , usa un segundo parametro de string []
     username = StringField('username',
                            [
                                validators.required('el usuario es requerido'),
                                validators.length(min=4, max=25, message="minimo 4 y max 25")
 
-                            ])
+                           ])
     email = EmailField('email'
                        )
     comment = TextField('Comentario')
 
-    #campo oculto para atacantes
-    honeypot = TextField('',[length_honeypot])
+    # campo oculto para atacantes
+    honeypot = TextField('', [length_honeypot])
 
 
+class LoginForm(Form):
+    username = StringField('username',
+                           [
+                               validators.required('el usuario es requerido'),
+                               validators.length(min=4, max=25, message="minimo 4 y max 25")
+
+                           ])
+    password = PasswordField('Password', [validators.required(message='El password es requerido')])
--- flask/estaticos/main.py
+++ flask/estaticos/main.py
@@ -2,10 +2,15 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+
+
 #se agrega el campo del py creado
 import forms
-
+from flask_wtf.csrf import CSRFProtect
 app = Flask(__name__,static_folder='templates/static')#nuevo objeto con name
+app.secret_key = 'hola'
+csrf = CSRFProtect(app)
+
 
 
 @app.route('/',methods=['POST','GET']) #recibe un decorador con un string, la ruta es base
@@ -22,7 +27,11 @@ def index():
     return render_template('index.html',title=title, form = comment_form)
 
 
+@app.route('/login',methods=['POST','GET'])
+def login():
+    login_form = forms.LoginForm()
 
+    return render_template('login.html', form = login_form)
 
 if __name__ == '__main__':
     app.run(debug=True,port=8000) # debug y puerto
